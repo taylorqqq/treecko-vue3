0f290adc272cca1fec8da953192b2b55148ffa09
Treecko <h17683820991@163.com>
2023-03-07 00:25:16 +0800

    子路由约定式

--- src/router/autoload.ts
+++ src/router/autoload.ts
@@ -1,20 +1,24 @@
 import { RouteRecordRaw } from "vue-router";
 
 const layouts = import.meta.globEager("../layout/*.vue");
+const views = import.meta.globEager("../views/**/*.vue");
 
 function getRoutes() {
   const layoutRoutes = [] as RouteRecordRaw[];
   Object.entries(layouts).forEach(([path, module]) => {
     const route = getRouteByModule(path, module);
+    route.children = getChildrenRoutes(route);
     layoutRoutes.push(route);
   });
-
   return layoutRoutes;
 }
 
 function getRouteByModule(file: string, module: { [key: string]: any }) {
-  const path = file.match(/\.\/(.*)\.vue$/)?.[1] as string;
-  const name = path.split("/")[1];
+  const modulePath = file.match(/\.\/(.*)\.vue$/)?.[1] as string;
+  // 找出第一个/的位置
+  const index = modulePath.indexOf("/");
+  // 截取最后一个/后面的字符串
+  const name = modulePath.substring(index + 1);
 
   const route = {
     path: `/${name}`,
@@ -25,4 +29,15 @@ function getRouteByModule(file: string, module: { [key: string]: any }) {
   return route;
 }
 
+function getChildrenRoutes(layoutRoute: RouteRecordRaw) {
+  const routes = [] as RouteRecordRaw[];
+  Object.entries(views).forEach(([path, module]) => {
+    if (path.includes(`../views/${layoutRoute.name as string}`)) {
+      const route = getRouteByModule(path, module);
+      routes.push(route);
+    }
+  });
+
+  return routes;
+}
 export default getRoutes();
---
+++ src/views/member/mail.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>mail</div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from "vue";
+</script>
