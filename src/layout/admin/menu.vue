689006bfdde88131141f70bc637485a8450dd105
Treecko <h17683820991@163.com>
2023-04-04 22:30:33 +0800

    优化菜单

--- src/layout/admin/menu.vue
+++ src/layout/admin/menu.vue
@@ -1,74 +1,83 @@
 <template>
-  <div class="menu" :class="{ close: useMenuStore().close }">
-    <div class="logo">
-      <i class="fab fa-battle-net text-pink-700 mr-2 text-[26px]"></i>
-      <span class="text-[20px]">管理系统</span>
-    </div>
+  <div class="admin-menu">
+    <div class="menu" :class="{ close: useMenuStore().close }">
+      <div class="logo">
+        <i class="fas fa-robot text-pink-700 mr-2 text-[26px]"></i>
+        <span class="text-[20px]">管理系统</span>
+      </div>
 
-    <!-- 菜单 -->
-    <div class="left-container text-white">
-      <dl>
-        <dt
-          @click="
-            () => {
-              $router.push('/dashboard');
-              menus.forEach((item) => {
-                item.isActive = false;
-                if (item.children && item.children.length > 0) {
-                  item.children.forEach((citem) => {
-                    citem.isActive = false;
-                  });
-                }
-              });
-            }
-          "
-          :class="{
-            active: $route.path === '/dashboard' && isMenuNotActive(),
-          }"
-        >
-          <section>
-            <i class="fas fa-home"></i>
-            <span>dashboard</span>
-          </section>
-        </dt>
-      </dl>
-      <dl v-for="(pmenu, index) in menus" :key="index">
-        <dt
-          @click="handeleChoosePMenu(pmenu)"
-          :class="{
-            active: pmenu?.isActive && useMenuStore().close,
-          }"
-        >
-          <section>
-            <i :class="pmenu?.icon"></i>
-            <span>{{ pmenu?.title }}</span>
-          </section>
-          <section>
-            <i
-              class="fas fa-angle-down duration-100 rotate"
-              :class="{ 'rotate-180': pmenu?.isActive }"
-            ></i>
-          </section>
-        </dt>
-        <dd
-          v-show="pmenu?.isActive"
-          v-for="(cmenu, cindex) in pmenu.children"
-          :class="{ active: cmenu?.isActive }"
-          @click="handeleChooseCMenu(pmenu, cmenu)"
-          :style="getStyle(cindex)"
-        >
-          <!-- {{ useMenuStore().close ? cmenu?.title?.slice(0, 4) : cmenu?.title }} -->
-          {{ cmenu?.title }}
-        </dd>
-      </dl>
+      <!-- 菜单 -->
+      <div class="left-container text-white">
+        <dl>
+          <dt
+            @click="
+              () => {
+                $router.push('/dashboard');
+                menus.forEach((item) => {
+                  item.isActive = false;
+                  if (item.children && item.children.length > 0) {
+                    item.children.forEach((citem) => {
+                      citem.isActive = false;
+                    });
+                  }
+                });
+              }
+            "
+            :class="{
+              active: $route.path === '/dashboard' && isMenuNotActive(),
+            }"
+          >
+            <section>
+              <i class="fas fa-home"></i>
+              <span>dashboard</span>
+            </section>
+          </dt>
+        </dl>
+        <dl v-for="(pmenu, index) in menus" :key="index">
+          <dt
+            @click="handeleChoosePMenu(pmenu)"
+            :class="{
+              pactive: pmenu?.isActive && useMenuStore().close,
+            }"
+          >
+            <section>
+              <i :class="pmenu?.icon"></i>
+              <span>{{ pmenu?.title }}</span>
+            </section>
+            <section>
+              <i
+                class="fas fa-angle-down duration-100 rotate"
+                :class="{ 'rotate-180': pmenu?.isActive }"
+              ></i>
+            </section>
+          </dt>
+          <dd v-show="pmenu?.isActive">
+            <div
+              v-for="(cmenu, cindex) in pmenu.children"
+              :class="{ active: cmenu?.isActive, horizontal: model.horizontal }"
+              @click="handeleChooseCMenu(pmenu, cmenu)"
+            >
+              {{ model.horizontal ? cmenu?.title : cmenu?.title?.slice(0, 4) }}
+              <!-- {{ cmenu?.title }} -->
+            </div>
+          </dd>
+        </dl>
+      </div>
     </div>
+    <!-- 遮罩层 -->
+    <div class="bg block md:hidden"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
+import { ref } from "vue";
 import { useMenuStore } from "@/store/menuStore";
 import router from "@/router";
 const { menus } = useMenuStore();
+
+const model = ref({
+  horizontal: true,
+});
 
 /**
  * @description 选择菜单
@@ -104,16 +113,6 @@ const handeleResetMenu = (menu: IMenu, status: number) => {
   });
 };
 
-/**
- * @description 获取子菜单样式
- */
-const getStyle = (index: number) => {
-  const top = index * 46 + "px"; // 40是菜单项的高度
-  return {
-    top,
-  };
-};
-
 const isMenuNotActive = () => {
   return menus.every((pmenu) => {
     return pmenu.isActive == false;
@@ -123,7 +122,7 @@ const isMenuNotActive = () => {
 
 <style scoped lang="scss">
 .menu {
-  @apply hidden md:block w-[200px] bg-gray-800 p-2 min-h-screen duration-300;
+  @apply w-[200px] bg-gray-800 p-2 min-h-screen duration-300;
   .logo {
     @apply text-gray-300 flex items-center;
   }
@@ -152,52 +151,78 @@ const isMenuNotActive = () => {
         &.active {
           @apply bg-blue-800;
         }
+        &.pactive {
+          @apply md:bg-blue-800;
+        }
       }
 
       dd {
-        @apply py-3 pl-4 my-2 rounded-md cursor-pointer bg-gray-700 hover:bg-blue-500 duration-300;
-        &.active {
-          @apply bg-blue-800 hover:bg-blue-500;
+        div {
+          @apply py-3 pl-4 my-2 rounded-md cursor-pointer bg-gray-700 hover:bg-blue-500 duration-300;
+          &.active {
+            @apply bg-blue-800 hover:bg-blue-500;
+          }
         }
       }
     }
   }
+}
 
-  &.close {
-    @apply w-[80px];
-    .logo {
-      @apply justify-center;
-    }
+@media screen and (max-width: 768px) {
+  .menu {
+    @apply absolute top-0 left-0 z-20;
+  }
+}
 
-    span {
-      @apply hidden;
-    }
+@media screen and (min-width: 768px) {
+  .menu {
+    &.close {
+      @apply w-[80px];
+      .logo {
+        @apply justify-center;
 
-    .left-container {
-      dl {
-        @apply relative;
-        dt {
-          @apply justify-center;
-          section {
-            i {
-              @apply mr-0;
+        i {
+          @apply mr-0;
+        }
+      }
+
+      span {
+        @apply hidden;
+      }
+
+      .left-container {
+        dl {
+          @apply relative;
+          dt {
+            @apply justify-center;
+            section {
+              i {
+                @apply mr-0;
+              }
+
+              .rotate {
+                @apply hidden;
+              }
             }
-
-            .rotate {
-              @apply hidden;
+          }
+          // &:hover {
+          //   dd {
+          //     display: block !important;
+          //   }
+          // }
+          dd {
+            @apply absolute w-[184px] left-[72px] top-0 m-0 z-10 bg-blue-800 px-2 bg-gray-900;
+            .horizontal {
+              @apply hover:bg-blue-500;
             }
           }
         }
-        // &:hover {
-        //   dd {
-        //     display: block !important;
-        //   }
-        // }
-        dd {
-          @apply absolute w-[184px] left-[80px] top-0 m-0 z-10;
-        }
       }
     }
   }
+}
+
+.bg {
+  @apply w-full h-full bg-black opacity-50 fixed top-0 left-0 z-10;
 }
 </style>
